644dfae2dbe9f9112b37da2981b8c10ad18dad24
ShuchangXu <676704276@qq.com>
2024-02-19 15:11:12 +0800

    extract_single_photo_info_mtree

--- Reviver_Proactive/build_m_tree.py
+++ Reviver_Proactive/build_m_tree.py
@@ -7,11 +7,7 @@ if __name__ == "__main__":
     load_dotenv()
     api_key=os.getenv("OPENAI_API_KEY")
     Processor = Photo_PreProcessor(api_key, "wmq_1")
-    
-    event_extraction_guide = open("./prompts/photo_event_guide", 'r', encoding='utf-8').read()
-    des_extraction_guide = open("./prompts/photo_des_guide", 'r', encoding='utf-8').read()
-    
 
-    for i in range(1, 30):
-        reply= Processor.visual_question_answer("",i)
+    Processor.batch_photo_extraction(4, 1)
+    Processor.save_m_tree()
         # Processor.save_reply(reply, photo_name)
--- Reviver_Proactive/pre_processor.py
+++ Reviver_Proactive/pre_processor.py
@@ -24,19 +24,46 @@ def json_parser(content):
     return json_response
 
 class Photo_PreProcessor:
-    def __init__(self, api_key, user):
+    def __init__(self, api_key, user, old_mtree_file = None, has_meta_data = False):
         self.client = OpenAI(api_key=api_key, timeout=TIME_OUT)
 
         self.user = user
         self.photo_dir = os.path.join(SCRIPT_DIR, "photos", self.user)
+        self.meta_data_list = None
+
+        if has_meta_data:
+            meta_data_json = json.loads(open(os.path.join(self.photo_dir, "meta_data.json"), 'r', encoding='utf-8').read())
+            self.meta_data_list = meta_data_json["meta_data"]
+
         self.json_dir = os.path.join(SCRIPT_DIR, "mtree_json", self.user)
+        self.mtree_save_path = os.path.join(self.json_dir, "memory_tree.json")
 
-        self.single_photo_prompt = open(os.path.join(SCRIPT_DIR, "prompts", "single_photo_extraction"), 'r', encoding='utf-8').read()
+        if not os.path.exists(self.json_dir):
+            os.makedirs(self.json_dir)
 
-        self.mtree = {}
-        self.mtree["ptext_list"] = []
-        self.mtree["photos"] = []
+        if old_mtree_file is None:
+            self.mtree = {
+                "super_event":"",
+                "events": [],
+                "shorts": [],
+                "photos": [],
+                "topics": [],
+                "ptexts": []
+            }
+        else:
+            self.m_tree = json.loads(open(os.path.join(self.json_dir, old_mtree_file), 'r', encoding='utf-8').read())
         
+        self.single_photo_prompt = open(os.path.join(SCRIPT_DIR, "prompts", "single_photo_extraction"), 'r', encoding='utf-8').read()
+        self.slicing_prompt = open(os.path.join(SCRIPT_DIR, "prompts", "slicing"), 'r', encoding='utf-8').read()
+    
+    def save_m_tree(self, save_path=None): 
+        if not save_path:
+            save_path = self.mtree_save_path
+        
+        with open(save_path, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(self.mtree, ensure_ascii=False, indent=4))
+        print("mtree已保存至", save_path)
+    
     def call_llm(self, content, max_tokens = MAX_TOKENS, model="gpt-4-vision-preview"):
         response = self.client.chat.completions.create(
             messages = content,
@@ -51,61 +78,79 @@ class Photo_PreProcessor:
         with open(image_path, "rb") as image_file:
             return base64.b64encode(image_file.read()).decode('utf-8')
         
-    def vqa(self, task, pid_list):
+    def vqa(self, prompt, pid, meta_data=""):
         current_content = [{
             "type": "text",
-            "text": task,
+            "text": prompt,
         }]
         base64_usr = self.encode_image(os.path.join(self.photo_dir, "_.jpeg"))
         current_content.append({
-                "type": "image_url",
-                "image_url": {
-                    "url": f"data:image/jpg;base64,{base64_usr}"
-                }
-            })
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpg;base64,{base64_usr}"
+            }
+        })
         
-        for pid in pid_list:
-            base64_img = self.encode_image(os.path.join(self.photo_dir, str(pid)+".jpeg"))
-                
-            current_content.append({
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpg;base64,{base64_img}"
-                    }
-                })
+        base64_img = self.encode_image(os.path.join(self.photo_dir, str(pid)+".jpeg"))
             
+        current_content.append({
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpg;base64,{base64_img}"
+            }
+        })
+
+        current_content.append({
+            "type": "text",
+            "text": "the meta data of the second picture is:" + meta_data
+        })
+
         content = [
             {"role": "user", "content": current_content}
         ]
 
         reply_string = self.call_llm(content)
+        console_log(reply_string)
 
         reply_dict = json_parser(reply_string)
 
         return reply_dict
-
     
     def single_photo_extraction(self, pid):
-        reply_dict = self.vqa(self.single_photo_prompt, [pid])
+        meta_data = ""
+        if self.meta_data_list is not None:
+            meta_data = self.meta_data_list[pid]
+
+        reply_dict = self.vqa(self.single_photo_prompt, pid, meta_data)
+
         return reply_dict
     
-    def all_photo_extraction(self, pid_bgn, p_cnt):
-        self.mtree["ptext_list"] = []
+    def batch_photo_extraction(self, pid_bgn, p_cnt):
+        self.mtree["ptexts"] = []
         for pid in range(pid_bgn, pid_bgn + p_cnt):
             ptext = self.single_photo_extraction(pid)
-            self.mtree["ptext_list"].append(ptext)
+            self.mtree["ptexts"].append(ptext)
+
+    # def slice(self):
+    #     content = []
+    #     task = self.slicing_prompt + json.dumps(self.mtree['ptexts'])
+    #     content.append({"role": "user", "content": task})
+
+    #     reply_string = self.call_llm(content)
+
+    #     idx_bgn_list = [int(e) if e.isdigit() else e for e in reply_string.split(',')]
+    #     idx_end_list = [for e in idx_bgn_list][1:]
+    #     idx_end_list.append(len(self.mtree['ptexts']))
 
 
-    def slice(self):
-        self.mtree["ptext_list"]
-        p_
-        if
-        return 
+
+    #     return reply_dict
+
         
     
-    def build_m_tree(self, ):
-        events = []
-        shorts = []
-        photos = []
-        topics = []
-        return 
+    # def build_m_tree(self, ):
+    #     events = []
+    #     shorts = []
+    #     photos = []
+    #     topics = []
+    #     return 

cfc8e21c49ccba688129b05fb867900445b020e5
ShuchangXu <676704276@qq.com>
2024-02-22 16:05:13 +0800

    zhy_1_ptext

--- Reviver_Proactive/build_m_tree.py
+++ Reviver_Proactive/build_m_tree.py
@@ -6,12 +6,12 @@ from pre_processor import Photo_PreProcessor
 if __name__ == "__main__":
     load_dotenv()
     api_key=os.getenv("OPENAI_API_KEY")
-    Processor = Photo_PreProcessor(api_key, "lmq_1", "memory_tree.json")
+    Processor = Photo_PreProcessor(api_key, "zhy_1", "memory_tree.json")
 
-    # Processor.batch_photo_extraction(1, 56)
+    # Processor.batch_photo_extraction(1, 33)
     # Processor.save_m_tree()
 
     # Processor.slice()
 
-    slice_locs = [1,4,8,14,19,26,29,32,37,42,46,52,57]
+    slice_locs = [1,2,6,8,11,15,17,21,26,33,33]
     Processor.build_m_tree(slice_locs)

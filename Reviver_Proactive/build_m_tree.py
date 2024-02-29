d5261f6435db769a108dfa9d055afddf0f43d20a
ShuchangXu <676704276@qq.com>
2024-03-01 00:39:07 +0800

    hjs

--- Reviver_Proactive/build_m_tree.py
+++ Reviver_Proactive/build_m_tree.py
@@ -6,13 +6,13 @@ from pre_processor import Photo_PreProcessor
 if __name__ == "__main__":
     load_dotenv()
     api_key=os.getenv("OPENAI_API_KEY")
-    Processor = Photo_PreProcessor(api_key, "zlj_2", has_meta_data=False)#, old_mtree_file="memory_tree.json")
+    Processor = Photo_PreProcessor(api_key, "hjs_2", has_meta_data=False, old_mtree_file="memory_tree.json")
 
     # Processor.single_photo_exact_location(51)
-    # Processor.batch_photo_extraction(1, 61)
+    # Processor.batch_photo_extraction(26, 8)
     # Processor.save_m_tree()
 
     # Processor.slice()
 
-    # slice_locs = [1,7,14,21,23,33,36,41,44]
-    # Processor.build_m_tree(slice_locs)
+    slice_locs = [1,9,14,18,24,26,28,34]
+    Processor.build_m_tree(slice_locs)

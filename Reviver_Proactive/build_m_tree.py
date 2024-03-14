b911ee037bf5c0d417253f20e899f9567e95cb76
CeraChen <1059315177@qq.com>
2024-03-14 10:55:35 +0800

    original wl_1 and wl_2 descriptions

--- Reviver_Proactive/build_m_tree.py
+++ Reviver_Proactive/build_m_tree.py
@@ -6,19 +6,19 @@ from pre_processor import Photo_PreProcessor
 if __name__ == "__main__":
     load_dotenv()
     api_key=os.getenv("GPT_API_KEY")
-    user = "zouchao_2"
+    user = "wl_2"
     
     
     ### Generate photo descriptions
-    # Processor = Photo_PreProcessor(api_key, user, has_meta_data=False)
-    # Processor.batch_photo_extraction(1, 1)
-    # Processor.save_m_tree()
+    Processor = Photo_PreProcessor(api_key, user, has_meta_data=False)
+    Processor.batch_photo_extraction(1, 19)
+    Processor.save_m_tree()
     
     
     ### Slice photos and build m_tree
-    Processor = Photo_PreProcessor(api_key, user, has_meta_data=False, old_mtree_file="memory_tree.json")
-    slice_locs = [1, 3, 7, 11, 14, 18, 21]
-    Processor.build_m_tree(slice_locs)
+    # Processor = Photo_PreProcessor(api_key, user, has_meta_data=False, old_mtree_file="memory_tree.json")
+    # slice_locs = [1, 3, 7, 11, 14, 18, 21]
+    # Processor.build_m_tree(slice_locs)
     
     
 

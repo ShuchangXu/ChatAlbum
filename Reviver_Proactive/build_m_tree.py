18fdc99274a0cfa94760b477f33df5d93e024d48
CeraChen <1059315177@qq.com>
2024-03-14 11:19:00 +0800

    original wl_2 mtree

--- Reviver_Proactive/build_m_tree.py
+++ Reviver_Proactive/build_m_tree.py
@@ -10,15 +10,15 @@ if __name__ == "__main__":
     
     
     ### Generate photo descriptions
-    Processor = Photo_PreProcessor(api_key, user, has_meta_data=False)
-    Processor.batch_photo_extraction(1, 19)
-    Processor.save_m_tree()
+    # Processor = Photo_PreProcessor(api_key, user, has_meta_data=False)
+    # Processor.batch_photo_extraction(1, 19)
+    # Processor.save_m_tree()
     
     
     ### Slice photos and build m_tree
-    # Processor = Photo_PreProcessor(api_key, user, has_meta_data=False, old_mtree_file="memory_tree.json")
-    # slice_locs = [1, 3, 7, 11, 14, 18, 21]
-    # Processor.build_m_tree(slice_locs)
+    Processor = Photo_PreProcessor(api_key, user, has_meta_data=False, old_mtree_file="memory_tree.json")
+    slice_locs = [1, 3, 7, 9, 12, 15, 18, 20]
+    Processor.build_m_tree(slice_locs)
     
     
 
